240c39a905a185050d1498c0fe62259d72f7754a
Sergei <sergokondrashevskij@ukr.net>
2020-02-28 14:37:18 +0200

    Sectoin Rewievs

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,26 @@
 $(window).ready(function() {
 
-	let roomsSlider = $('.rooms-slider');
+	let roomsSlider  = $('.rooms-slider'),
+			rewiewsSlider = $('.rewievs-slider');
 
 	roomsSlider.slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		infinite: true,
 		dots: true,
-		dotsClass: 'rooms-slider__dots',
-		prevArrow: $('.left-arrow'),
-		nextArrow: $('.right-arrow'),
+		dotsClass: 'slider__dots',
+		prevArrow: $('.rooms-left__arrow'),
+		nextArrow: $('.rooms-right__arrow'),
+	});
+
+	rewiewsSlider.slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		infinite: true,
+		dots: true,
+		dotsClass: 'slider__dots',
+		prevArrow: $('.rewievs-left__arrow'),
+		nextArrow: $('.rewievs-right__arrow'),
 	});
 
 });
